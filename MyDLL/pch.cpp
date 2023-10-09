d51d84293778677a589e11bf893cb76c392da541
Egor251 <egorpyshny@gmail.com>
2023-10-09 16:29:21 +0300

    Добавьте файлы проекта.

---
+++ MyDLL/pch.cpp
@@ -0,0 +1,23 @@
+﻿// pch.cpp: файл исходного кода, соответствующий предварительно скомпилированному заголовочному файлу
+
+#include "pch.h"
+#include <iostream>
+
+using namespace std;
+
+int add(int a, int b)
+{
+	return a + b;
+}
+
+int sub(int a, int b)
+{
+	return a - b;
+}
+
+void printFunction(string message)
+{
+	cout << message << endl;
+}
+
+// При использовании предварительно скомпилированных заголовочных файлов необходим следующий файл исходного кода для выполнения сборки.
